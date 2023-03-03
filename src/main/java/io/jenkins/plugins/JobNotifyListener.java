d8519d7b3201622e839843576fad69808e2e053d
huisunan <571101320@qq.com>
2023-03-04 00:06:50 +0800

    ✨ init

--- src/main/java/io/jenkins/plugins/JobNotifyListener.java
+++ src/main/java/io/jenkins/plugins/JobNotifyListener.java
@@ -1,2 +1,19 @@
-package io.jenkins.plugins;public class JobNotifyListener {
+package io.jenkins.plugins;
+
+import hudson.Extension;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.model.listeners.RunListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Extension
+public class JobNotifyListener extends RunListener<Run<?, ?>> {
+    private final static Logger log = LoggerFactory.getLogger(JobNotifyListener.class);
+
+    @Override
+    public void onStarted(Run<?, ?> run, TaskListener listener) {
+        log.debug("监听到了任务启动:{}",run.getId());
+        NotifyBuilder builder = new NotifyBuilder("echo 123");
+    }
 }
--- src/main/java/io/jenkins/plugins/NotifyBuilder.java
+++ src/main/java/io/jenkins/plugins/NotifyBuilder.java
@@ -1,2 +1,35 @@
-package io.jenkins.plugins;public class NotifyBuilder {
+package io.jenkins.plugins;
+
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.tasks.Builder;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class NotifyBuilder extends Builder {
+
+    private final String command;
+
+    NotifyBuilder(String command) {
+        this.command = command;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        PrintStream logger = listener.getLogger();
+        try {
+            logger.println("executing command :" + command);
+            int result = launcher.launch().cmds(command.split(" ")).stdout(logger).join();
+            if (result != 0) {
+                logger.println("Command failed with " + result + " exit code");
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            logger.println("Failed to execute command: " + e.getMessage());
+            return false;
+        }
+    }
 }
