0c54c324bd97fde29f862343faf6989b7b4b10a3
huisunan <571101320@qq.com>
2023-03-05 21:19:06 +0800

    ✨ 代码优化

--- src/main/java/io/jenkins/plugins/JobNotifyListener.java
+++ src/main/java/io/jenkins/plugins/JobNotifyListener.java
@@ -5,6 +5,7 @@ import hudson.Extension;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.model.listeners.RunListener;
+import hudson.util.LineEndingConversion;
 import jenkins.model.Jenkins;
 import org.apache.commons.io.IOUtils;
 
@@ -21,24 +22,41 @@ import java.util.stream.Collectors;
 @Extension
 public class JobNotifyListener extends RunListener<Run<?, ?>> {
 
+    /**
+     * 参考CommandInterpreter
+     *
+     * @param run      The started build.
+     * @param listener The listener for this build. This can be used to produce log messages, for example,
+     *                 which becomes a part of the "console output" of this build.
+     */
     @Override
     public void onStarted(Run<?, ?> run, TaskListener listener) {
         Jenkins jenkins = Jenkins.get();
         JobNotifyConfig notifyConfig = jenkins.getDescriptorByType(JobNotifyConfig.class);
         PrintStream logger = listener.getLogger();
         logger.println("任务全局监听");
-        String onStartedCmd = notifyConfig.getOnStartedCmd();
+        String command = notifyConfig.getOnStartedCmd();
+        File comandFile = null;
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.redirectErrorStream(true);
+        String convertCommand = null;
         try {
-            File batFile = File.createTempFile("jenkins", ".bat");
-            try (FileOutputStream fileOutputStream = new FileOutputStream(batFile)){
-                IOUtils.write(onStartedCmd,fileOutputStream, StandardCharsets.UTF_8);
+            if (isWindows()) {
+                comandFile = File.createTempFile("jenkins", ".bat");
+                convertCommand = LineEndingConversion.convertEOL(command + "\r\nexit %ERRORLEVEL%", LineEndingConversion.EOLType.Windows);
+                pb.command("cmd", "/c", comandFile.getAbsolutePath());
+            } else if (isUnix()) {
+                comandFile = File.createTempFile("jenkins", ".sh");
+            } else {
+                logger.println("未知的系统，通知跳过");
+                return;
             }
-            logger.println("执行命令:cmd /c " + batFile.getAbsolutePath());
-            //创建shell文件
-            ProcessBuilder pb = new ProcessBuilder(
-                    "cmd", "/c", batFile.getAbsolutePath()
-            );
-            pb.redirectErrorStream(true);
+            if (convertCommand == null) {
+                return;
+            }
+            try (FileOutputStream fileOutputStream = new FileOutputStream(comandFile)) {
+                IOUtils.write(convertCommand, fileOutputStream, StandardCharsets.UTF_8);
+            }
             Process p = pb.start();
             p.waitFor(); // 等待进程执行完成
             String s = IOUtils.toString(p.getInputStream(), Charset.defaultCharset());
@@ -50,8 +68,12 @@ public class JobNotifyListener extends RunListener<Run<?, ?>> {
             } else { // 执行失败
                 logger.println("Failed to execute the script.");
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (Exception e) {
             logger.println("execute error:" + e.getMessage());
+        } finally {
+            if (comandFile != null) {
+                comandFile.deleteOnExit();
+            }
         }
     }
 
@@ -69,5 +91,6 @@ public class JobNotifyListener extends RunListener<Run<?, ?>> {
         String osName = System.getProperty("os.name");
         return osName.toLowerCase().startsWith("windows");
     }
+
 
 }
